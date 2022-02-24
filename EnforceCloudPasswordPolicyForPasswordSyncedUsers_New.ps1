d49229f89593f55f396642d8f7f4026c4596d06b
Ivan <56428200+Durkadz@users.noreply.github.com>
2022-02-24 10:23:44 +0100

    Add files via upload

---
+++ EnforceCloudPasswordPolicyForPasswordSyncedUsers_New.ps1
@@ -0,0 +1,15 @@
+﻿#Connect to Azure
+Connect-AzureAD
+#Declarate Variables
+$GroupId = "2a970a5b-17d4-4ae0-941c-d58fd2578d4c"
+$Users =  Get-AzureADGroupMember -ObjectId $GroupId | Select ObjectId, PasswordPolicies
+#Get only users that don´t have property None in the PasswordPolicies attribute.
+ForEach($User in $Users) {
+   
+   If ($User.PasswordPolicies -ne "None"){
+       Set-AzureADUser -ObjectId $User.ObjectID -PasswordPolicies None
+       
+    }   
+
+}
+
